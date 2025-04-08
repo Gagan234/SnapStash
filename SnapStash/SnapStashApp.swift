7cac3021115abf8cc62ffb0ab7b0fd5842ac0693
Gagan <gagansapkota668@gmail.com>
2025-04-08 12:25:52 -0400

    Initial Commit

---
+++ SnapStash/SnapStashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapStashApp.swift
+//  SnapStash
+//
+//  Created by Work on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapStashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapStashTests/SnapStashTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapStashTests.swift
+//  SnapStashTests
+//
+//  Created by Work on 4/8/25.
+//
+
+import Testing
+@testable import SnapStash
+
+struct SnapStashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
